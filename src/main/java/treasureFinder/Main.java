2d06665cf5212c76581b9d844aeb515bbf663b3f
matned <mat.niedbal@yahoo.com>
2020-02-06 19:59:44 +0100

    - Maze generator - stack over flow catch

--- src/main/java/treasureFinder/Main.java
+++ src/main/java/treasureFinder/Main.java
@@ -1,10 +1,13 @@
 package treasureFinder;
 
+import treasureFinder.gameMechanics.Field;
+import treasureFinder.gameMechanics.LabirynthGenerator;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        LabirynthGenerator maze = new LabirynthGenerator(100,10);
     }
 }
 
-///prosta giera w swingu do  znajdywania skarbów
